640620d4a8fd5990bc99c5d5be86856f546a2271
wapec <waapec@gmail.com>
2019-04-29 16:45:30 +0300

    form

---
+++ src/app/components/index.ts
@@ -0,0 +1,11 @@
+import {FormComponent} from './form/form.component';
+import {FirstStepComponent} from './first-step/first-step.component';
+import {SecondStepComponent} from './second-step/second-step.component';
+import {ThirdStepComponent} from './third-step/third-step.component';
+
+export const components: any[] = [
+  FormComponent,
+  FirstStepComponent,
+  SecondStepComponent,
+  ThirdStepComponent
+];
